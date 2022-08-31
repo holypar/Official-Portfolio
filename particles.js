a8393dca69c735b02d02a54ceb6c50c1e8ba4e19
holypar <singhparminder9411@gmail.com>
2022-08-31 14:47:39 -0700

    tried to fix particles

--- particles.js
+++ particles.js
@@ -33,7 +33,7 @@ var pJS = function(tag_id, params){
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#554994'
+          color: '#fff'
         },
         polygon: {
           nb_sides: 5
